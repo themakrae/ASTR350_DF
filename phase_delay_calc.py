2233a1445264a02f1668aa70660b9f99ec0d87c0
Makenzi <mf6@wellesley.edu>
2022-05-27 16:52:56 -0400

    Updated correlation calculation.

--- phase_delay_calc.py
+++ phase_delay_calc.py
@@ -1,18 +1,7 @@
 from rtlsdr import RtlSdr
-from gnuradio import blocks
-from threading import Lock
-from gnuradio import gr
-from gnuradio.filter import firdes
-from grc_gnuradio import blks2
-from pylab import *
-from math import log,ceil
 from scipy import signal as sgn
 import csv
 import numpy as np
-import osmosdr
-import time
-import multi_rtl
-import threading
 
 # Read in sdr1 data
 with open('/home/pi/work/sdr_1_data.csv') as csv_file:
@@ -24,18 +13,20 @@ with open('/home/pi/work/sdr_2_data.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     sdr_2_data = row[0]
     
-# Declare phase delay list
+# Declarations
 phase_delays = []
+num_samp = len(sdr_1_data)/512
+x = 0
 
 # Calculate correlation and delay in data
-for i in sdr_1_data:
-    index = sdr_1_data.index(i)
-    sdr_1 = sdr_1_data[index]
-    sdr_2 = sdr_2_data[index]
+for i in range(num_samp):
+    sdr_1 = sdr_1_data[x:512+x]
+    sdr_2 = sdr_2_data[x:512+x]
     correlation = sgn.correlate(sdr_1-np.mean(sdr_1), sdr_2 - np.mean(sdr_2), mode="full")
     delays = sgn.correlation_lags(len(sdr_1), len(sdr_2), mode="full")
     delay = delays[np.argmax(abs(correlation))]
     phase_delays.append(delay)
+    x+=512
 
 # Create and write data to file
 with open('phase_delays.csv', 'w', newline='') as f:
